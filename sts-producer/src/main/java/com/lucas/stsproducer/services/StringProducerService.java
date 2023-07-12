d854b89e5a8f27168fd7453eef07a1363aee4391
Lucas Ramos <kawasaki.ryuji8@gmail.com>
2023-07-12 19:32:40 -0300

    Foi adicionado callback

--- sts-producer/src/main/java/com/lucas/stsproducer/services/StringProducerService.java
+++ sts-producer/src/main/java/com/lucas/stsproducer/services/StringProducerService.java
@@ -1,9 +1,14 @@
 package com.lucas.stsproducer.services;
 
 import lombok.RequiredArgsConstructor;
+import lombok.extern.log4j.Log4j2;
 import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.kafka.support.SendResult;
 import org.springframework.stereotype.Service;
 
+import java.util.concurrent.CompletableFuture;
+
+@Log4j2
 @RequiredArgsConstructor
 @Service
 public class StringProducerService {
@@ -11,6 +16,20 @@ public class StringProducerService {
     private final KafkaTemplate<String, String> kafkaTemplate;
 
     public void sendMessages(String message){
-        kafkaTemplate.send("str-topic", message);
+
+        CompletableFuture<SendResult<String, String>> future = kafkaTemplate.send("str-topic", message);
+
+        future.whenComplete((result, ex) -> {
+            if (ex != null) {
+                log.error("Error sending message: {}", ex.getMessage());
+                return;
+            }
+            log.info("Message sent successfully: {}", result.getProducerRecord().value());
+            log.info(
+                    "Partition {}, Offset {}",
+                    result.getRecordMetadata().partition(),
+                    result.getRecordMetadata().offset()
+            );
+        });
     }
 }
